860ebf233889894c455d00c68f944467d9a1f972
yacalder <yacalder@bancolombia.com.co>
2019-04-28 18:40:28 -0500

    adding resources and config Fragments

--- app/src/main/java/com/example/yacalder/wikipedia/activities/MainActivity.kt
+++ app/src/main/java/com/example/yacalder/wikipedia/activities/MainActivity.kt
@@ -1,40 +1,49 @@
 package com.example.yacalder.wikipedia.activities
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import com.example.yacalder.wikipedia.R
+import com.example.yacalder.wikipedia.fragments.ExploreFragment
+import com.example.yacalder.wikipedia.fragments.FavoriteFragment
+import com.example.yacalder.wikipedia.fragments.HistoryFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val exploreFragment: ExploreFragment
+    private val favoriteFragment: FavoriteFragment
+    private val historyFragment: HistoryFragment
+
+    init {
+        exploreFragment = ExploreFragment()
+        favoriteFragment = FavoriteFragment()
+        historyFragment = HistoryFragment()
+    }
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+
+        when(item.itemId){
+            R.id.navigation_explore -> transaction.replace(R.id.fragment_container, exploreFragment)
+            R.id.navigation_favorite -> transaction.replace(R.id.fragment_container, favoriteFragment)
+            R.id.navigation_history -> transaction.replace(R.id.fragment_container, historyFragment)
         }
-        false
+
+        transaction.commit()
+        true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        message.setOnClickListener{ _ ->
-            startActivity(Intent(this, ArticleDetailActivity::class.java))
-        }
-
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragment_container, exploreFragment)
+        transaction.commit()
     }
 }
